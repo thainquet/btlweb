ea4eaa669379758034a2a64c92dbc53d81ba67be
Nguyen Quang Thai <thainquet@github.com>
2018-10-21 23:10:40 +0700

    fix port part 2

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const path = require('path');
 const mysql = require('mysql');
 let public = path.join(__dirname, 'public');
 
-app.set('PORT', process.env.PORT || 7777);
 const app = express();
 //app.use(express.static(__dirname));	
+
+
+app.set('PORT', process.env.PORT || 7777);
+
+
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.sendFile(path.join(public, '/view/login.view.html'));
