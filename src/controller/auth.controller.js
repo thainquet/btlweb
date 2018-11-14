65b0c79f18e4349c590d3c6825f8a1c9bcf468f4
Lê Văn Huy <huylv@talent.ominext.com>
2018-11-14 15:44:34 +0700

    noChange

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -19,10 +19,10 @@ var auth = {
             success: true,
             data: {
               // Them id cua Quynh vao cho nay
-              id: data[0].id,
-              username: data[0].username,
-              isAdmin: data[0].isAdmin,
-              isTeacher: data[0].isTeacher
+            id: data[0].id,
+            username: data[0].username,
+            isAdmin: data[0].isAdmin,
+            isTeacher: data[0].isTeacher
             }
           })
         } else {
